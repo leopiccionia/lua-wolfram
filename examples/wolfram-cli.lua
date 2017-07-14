32460a1822cd522b3684de880d0f3a5ada0fddc3
leopiccionia <leopiccionia@gmail.com>
2017-07-14 16:29:09 -0300

    Indenting using spaces.

--- examples/wolfram-cli.lua
+++ examples/wolfram-cli.lua
@@ -10,19 +10,19 @@ local WolframAlpha = require 'wolfram'
 local wolfram = WolframAlpha('YOUR-API-KEY-HERE')
 
 if not args.full then
-	local body = assert(wolfram:query(args.query))
-	print(body)
+    local body = assert(wolfram:query(args.query))
+    print(body)
 else
-	local body = assert(wolfram:full_query(args.query))
-	for key, pod in pairs(body) do
-		print(key .. ':')
-		for subkey, subpod in pairs(pod) do
-			if type(subkey) == 'string' then
-				print('\t' .. subkey .. ':')
-				print('\t\t' .. subpod)
-			else
-				print('\t' .. subpod)
-			end
-		end
-	end
+    local body = assert(wolfram:full_query(args.query))
+    for key, pod in pairs(body) do
+        print(key .. ':')
+        for subkey, subpod in pairs(pod) do
+            if type(subkey) == 'string' then
+                print('\t' .. subkey .. ':')
+                print('\t\t' .. subpod)
+            else
+                print('\t' .. subpod)
+            end
+        end
+    end
 end
